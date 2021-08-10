c26293b8360dbdacf05c0ca095bae5b98d1854ec
Ahmad Salman Malik <malik.salman96@gmail.com>
2021-08-10 16:06:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,218 @@
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+//import { withStyles } from '@material-ui/core/styles';
+import Badge from '@material-ui/core/Badge';
+import { useState } from "react";
+import { Kart } from './Kart';
+//import { CenterFocusStrong } from '@material-ui/icons';
+function  App() {
 
-function App() {
+
+  const abckart = [
+    {
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    productType: 'Fancy Products',
+    offerPrice: '$40.00 - $80.00',    
+    },
+    {
+      image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    tag: "Sale",
+    productType: 'Special Item',
+    offerPrice:  '$18.00',
+    price: '$20.00',
+    rating: '⭐⭐⭐⭐⭐',   
+    },
+    {
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    tag: 'Sale',
+    productType: 'Sale Item',
+    offerPrice: '$25.00',
+    price: '$50.00',  
+    },
+    {
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    productType: 'Popular Item', 
+    offerPrice: '$40.00',
+        rating: '⭐⭐⭐⭐⭐'  
+      },
+      {
+        image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        tag: 'Sale',
+        productType: 'Sale Item',
+        offerPrice: '$25.00',
+        price: '$50.00',
+
+      },
+      {
+        image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+        productType: 'Fancy Products',
+        offerPrice: '$120.00 - $280.00',    
+        },
+        {
+          image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+          tag: 'Sale',
+          productType: 'Special Item',
+          offerPrice:  '$18.00',
+          price: '$20.00',
+          rating: '⭐⭐⭐⭐⭐',   
+          },
+          {
+            image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+            productType: 'Popular Item', 
+            offerPrice: '$40.00',
+                rating: '⭐⭐⭐⭐⭐'  
+              },
+  ];
+
+//   constructor(props){ 
+//     super(props);
+//     this.state = {
+//     cart:  0
+//   }
+
+//   this.updateState = this.updateState.bind(this);
+// }
+//   updateState(calculation) 
+//   {
+//     if(calculation === 'Clicked')
+//     {
+//       this.setState({cart: this.data + 1})
+//     }
+//   }
+  const [cart, setCart] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div> 
+        <h1>{cart}</h1>
+         <IconButton aria-label="cart">
+  <StyledBadge badgeContent={cart} color="secondary">
+    <ShoppingCartIcon />
+  </StyledBadge>
+</IconButton>
+      
+       <div className={'parent'}>
+      <Kart cart={cart} setCart={setCart} />
+        
+      {abckart.map(({image, productType, price, cart, setCart, offerPrice, rating }) => (<Kart cart={cart} setCart={setCart}  productImage={image} product={productType} price={price} offerPrice={offerPrice} rating = {rating}/>))}
+      
+    </div>
     </div>
   );
-}
+  }
+// function Vote(props){
+//   const [like, setLike] = useState(0);
+//   const [dislike, setDislike] = useState(0);
+//   console.log("Props",props);
+//   return(
+//     <div className={'App'}
+//     style={{
+//       background: like >= dislike ? "smokewhite" : "crimson",
+//     }}>
+//       <h1 style={{textTransform: "uppercase"}}>{props.brandName} </h1>
+//       <h4>{props.model}</h4>
+//       <Specification specs={props.specs}/>
+      
+
+//       <button onClick={() => setLike(like + 1)}> {like}</button>
+//       <button onClick={() => setDislike(dislike + 1)}>{dislike}</button>
+      
+
+//     </div>
+//   )
+// }
+
+
+
+
+export const useStyles = makeStyles({
+  root: {
+    width: 280,
+    height: 400,
+    textAlign: 'center',
+    marginBottom: 15,
+  },
+  media: {
+    height: 170,
+  },
+});
+
+const StyledBadge = withStyles((theme) => ({
+  badge: {
+    right: -3,
+    top: 13,
+    border: `2px solid ${theme.palette.background.paper}`,
+    padding: '0 4px',
+  },
+}))(Badge);
 
 export default App;
+
+
+
+// function Users(){
+//     return(
+//     <div>
+//         <h2>USER details</h2>
+//         </div>
+//     );
+// }
+
+
+
+// live pratice test
+{/* <Link to="/users">User Details</Link>
+<Route path="/users">
+    <User/>
+    </Route> */}
+
+// {users.map({name, image})=> <User userName={user} userImage={imgage}/>)}
+// function Users({userName, userImage}){
+//     return(
+//     <div>
+//         <img style={{borderRadius: 50%}} src={userImage}></img>
+//         <h2>{userName}</h2>
+//         </div>
+//     );
+// }
+
+//     <AddUser user={userName, userImage}/>
+    
+    
+//     function AddUser({userName, userImage}){
+//     return(
+//         <div>
+//         const [name, setName] = useState("");
+//         const [profile, setProfile] = useState("");
+        
+// <input value={print}
+// onChange = {(event)=> setName(event.target.value)}
+// type="text"
+// placeholder="Name"
+// />
+  
+
+// <input value={profile}
+// onChange = {(event)=> setProfile(event.target.value)}
+// type="text"
+// placeholder="Name"
+// />
+//     {profile}
+//           {print}
+// <button  onClick={()=>setUsers()}/>
+        
+//         </div>
+//     );
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Kart.js
@@ -0,0 +1,47 @@
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+//import { SettingsApplicationsRounded } from '@material-ui/icons';
+//import { useState } from "react";
+import { useStyles } from './App';
+
+export function Kart({price, rating, productImage, product, offerPrice, cart, setCart}) {
+  const classes = useStyles();
+  //const [cart, setCart] = useState(0);
+  //console.log("cart Value", cart);
+  const incrementCart = () => setCart(cart + 1);
+  return (
+    <div className={'cart-list'}>
+      <Card className={classes.root}>
+
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={productImage}
+            title="Product Image" />
+          <CardContent style={{ height: '8rem' }}>
+            <Typography gutterBottom variant="h5" component="h2" style={{ fontWeight: '700' }}>
+              {product}
+            </Typography>
+            <Typography>{offerPrice}</Typography>
+            <Typography style={{ textDecorationLine: 'line-through' }}>{price}</Typography>
+            <Typography>{rating}</Typography>
+          </CardContent>
+        </CardActionArea>
+
+        <CardActions>
+          <Button style={{ margin: '8px', alignItems: 'center' }} onClick={incrementCart} variant="outlined" size="medium">
+            Add to Cart
+      </Button>
+        </CardActions>
+
+      </Card>
+    </div>
+
+  );
+
+}
